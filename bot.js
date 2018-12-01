01d4392b339a68230761607e6d9eec67f5f6133f
badermc123 <43385932+badermc123@users.noreply.github.com>
2018-12-01 21:56:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,4 +58,30 @@ client.channels.get("491690814951522305").send(`${ReBeL[Math.floor(Math.random()
 
 });
 
+const lol =
+[
+'**Wellcome To Urgent:notes::notes:...**',
+'**Wellcome To Urgent:notes::notes:...**',
+'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
+'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
+'**Bienvenue à Urgent :zzz::musical_keyboard:**',
+'**Bienvenue à Urgent :zzz::musical_keyboard:**',
+'**Welcome To Urgent  :heart:**',
+'**Welcome To Urgent  :heart:**',
+'**Welcome To Urgent...:frowning: :heart: **',
+'**Welcome To Urgent...:frowning: :heart: **',
+'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
+'***Wellcome To Urgent:notes::notes:...**',
+'**Welcome To Urgent  :heart:**',
+'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**'
+]
+client.on('guildMemberAdd', member => {
+const codes = member.guild.channels.get("517790611776995330");
+if(!codes) return;
+if(codes) {
+    setTimeout(() => codes.send(`${lol[Math.floor(Math.random() * lol.length)]} ${member}`), 3000)       
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
