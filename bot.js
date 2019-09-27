9cdf6cabe2e7dfd3258c8aefcb6b2e8caf16ead4
badermc123 <43385932+badermc123@users.noreply.github.com>
2019-09-28 00:31:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,42 +46,33 @@ if (message.content.startsWith(adminprefix + 'sa')) {
 
 console.log("BOT ONLINE");
 
-  client.on('ready', async () => {
-
-      let ReBeL = ["Hiiiiبدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر iiiiiii","Looooبدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر ooool","frieenبدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر ds","badeeeeبدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر er","Kiiiiiiبدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر بدر iing","xDDDDDDDD"]
-
-  setInterval(() => {
-
-client.channels.get("4916908149515225").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-
-},40000);
-
 });
-
+client.on('ready', () => {
+    console.log(`Logged as ${client.user.tag}`)
+})
 const lol =
 [
-'**Wellcome To Urgent:notes::notes:...**',
-'**Wellcome To Urgent:notes::notes:...**',
-'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
-'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
-'**Bienvenue à Urgent :zzz::musical_keyboard:**',
-'**Bienvenue à Urgent :zzz::musical_keyboard:**',
-'**Welcome To Urgent  :heart:**',
-'**Welcome To Urgent  :heart:**',
-'**Welcome To Urgent...:frowning: :heart: **',
-'**Welcome To Urgent...:frowning: :heart: **',
-'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**',
-'***Wellcome To Urgent:notes::notes:...**',
-'**Welcome To Urgent  :heart:**',
-'**welcome to Urgent SERVER.. :wilted_rose::black_heart:**'
+'**Wellcome To Viólin:notes::notes:...**',
+'**Wellcome To Viólin:notes::notes:...**',
+'**welcome to Viólin SERVER.. :wilted_rose::black_heart:**',
+'**welcome to Viólin SERVER.. :wilted_rose::black_heart:**',
+'**Bienvenue à Viólin :zzz::musical_keyboard:**',
+'**Bienvenue à Viólin :zzz::musical_keyboard:**',
+'**Welcome To Viólin  :heart:**',
+'**Welcome To Viólin  :heart:**',
+'**Welcome To Viólin...:frowning: :heart: **',
+'**Welcome To Viólin...:frowning: :heart: **',
+'**welcome to Viólin SERVER.. :wilted_rose::black_heart:**',
+'***Wellcome To Viólin:notes::notes:...**',
+'**Welcome To Viólin  :heart:**',
+'**welcome to Viólin SERVER.. :wilted_rose::black_heart:**'
 ]
 client.on('guildMemberAdd', member => {
-const codes = member.guild.channels.get("5177906117799530");
+const codes = member.guild.channels.get("621423241608822804");
 if(!codes) return;
 if(codes) {
     setTimeout(() => codes.send(`${lol[Math.floor(Math.random() * lol.length)]} ${member}`), 3000)       
 }
 });
-
 
 client.login(process.env.BOT_TOKEN);
