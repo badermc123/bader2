9a0ae034d2c0d0a305042acf2d959d0f678e96d1
badermc123 <43385932+badermc123@users.noreply.github.com>
2019-09-28 00:39:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,7 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const adminprefix = "B+";
-const developers = ['476130473681092608'];
-
-console.log("Paaaaartly");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
-})
-
-
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**جاري تغيير الأفتار... :** `);
-}
-});
-
+let timer;
 console.log("BOT ONLINE");
-
-});
 client.on('ready', () => {
     console.log(`Logged as ${client.user.tag}`)
 })
@@ -74,5 +29,4 @@ if(codes) {
     setTimeout(() => codes.send(`${lol[Math.floor(Math.random() * lol.length)]} ${member}`), 3000)       
 }
 });
-
-client.login(process.env.BOT_TOKEN);
+client.login('NDc2MTMwNDczNjgxMDkyNjA4.XYZR0g.NGp8E_BrxmkssbG7TuwYKzniPRY');
