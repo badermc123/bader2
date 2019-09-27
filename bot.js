7db88f85bd5b80bc7aeffb96e0d384ea6acb4488
badermc123 <43385932+badermc123@users.noreply.github.com>
2019-09-28 00:48:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,34 @@
-  message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const adminprefix = B+;
+const developers = ['476130473681092608'];
+
+console.log(Paaaaartly);
+
+client.on('ready', () = {
+    console.log(`Logged as ${client.user.tag}`)
+})
+
+
+client.on('message', message = {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`تم تغيير البلاينق الى   ${argresult}`)
+  } else 
+     if (message.content === (adminprefix + leave)) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type'WATCHING'});
+      message.channel.send(`تَم تغيير الواتشينق الى   ${argresult}`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type'LISTENING'});
+      message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, httpswww.twitch.tvpaaaaaartly);
